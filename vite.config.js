0308aa39819b006f4035892efc6f7bb925a42b53
ikeogu <ikeogu322@gmail.com>
2025-08-06 22:57:47 +0100

    Add Vite production build

--- vite.config.js
+++ vite.config.js
@@ -3,14 +3,19 @@ import vue from '@vitejs/plugin-vue';
 import laravel from 'laravel-vite-plugin';
 
 export default defineConfig({
-  plugins: [
-    laravel({
-      input: ['resources/css/app.css', 'resources/js/app.js'],
-      refresh: true,
-    }),
-    vue(),
+    plugins: [
+        laravel({
+            input: ['resources/css/app.css', 'resources/js/app.js'],
+            refresh: true,
+        }),
+         vue()
     ],
     build: {
-    outDir: 'public/build',
-  },
+        manifest: true,
+        outDir: 'public/build',
+    },
+    base: '/build/', // This is important for correct path resolution
+    server: {
+        https: true,
+    }
 });
