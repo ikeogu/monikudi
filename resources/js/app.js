ae3efd257bcbdce5c57a185bc45d787f7c533c78
ikeogu <ikeogu322@gmail.com>
2025-08-06 15:52:47 +0100

    chores: complete

--- resources/js/app.js
+++ resources/js/app.js
@@ -5,21 +5,11 @@ import { createInertiaApp } from '@inertiajs/vue3';
 import { resolvePageComponent } from 'laravel-vite-plugin/inertia-helpers';
 import { createApp, h } from 'vue';
 import { ZiggyVue } from 'ziggy-js'; // ✅ FROM node_modules
-import { registerSW } from 'virtual:pwa-register';
+
 
 
 const appName = import.meta.env.VITE_APP_NAME || 'Laravel';
 
-const updateSW = registerSW({
-  onNeedRefresh() {
-    if (confirm("New content available. Refresh?")) {
-      updateSW(true)
-    }
-  },
-  onOfflineReady() {
-    console.log("App ready to work offline")
-  },
-})
 
 createInertiaApp({
     title: (title) => `${title} - ${appName}`,
--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,13 @@
-// vite.config.js or vite.config.ts
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
-
+import { defineConfig } from 'vite';
+import vue from '@vitejs/plugin-vue';
+import laravel from 'laravel-vite-plugin';
 
 export default defineConfig({
   plugins: [
+    laravel({
+      input: ['resources/css/app.css', 'resources/js/app.js'],
+      refresh: true,
+    }),
     vue(),
-
-  ]
-})
+  ],
+});
