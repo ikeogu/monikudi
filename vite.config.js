b5ad00119895ce88293286db2caebb249ff9b390
ikeogu <ikeogu322@gmail.com>
2025-08-06 15:41:18 +0100

    chores: complete

--- vite.config.js
+++ vite.config.js
@@ -1,34 +1,11 @@
 // vite.config.js or vite.config.ts
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
-import { VitePWA } from 'vite-plugin-pwa'
+
 
 export default defineConfig({
   plugins: [
     vue(),
-    VitePWA({
-      registerType: 'autoUpdate',
-      includeAssets: ['favicon.svg', 'robots.txt'],
-      manifest: {
-        name: 'Monikudi',
-        short_name: 'App',
-        start_url: '/',
-        display: 'standalone',
-        background_color: '#ffffff',
-        theme_color: '#42b883',
-        icons: [
-          {
-            src: '/img/pwa-192x192.png',
-            sizes: '192x192',
-            type: 'image/png'
-          },
-          {
-            src: '/pwa-512x512.png',
-            sizes: '512x512',
-            type: 'image/png'
-          }
-        ]
-      }
-    })
+
   ]
 })
